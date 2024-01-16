723ad34fc93fac9635703adbf877810a88d75203
tjqwn <tjqwn@ahn-laptop>
2024-01-16 09:17:46 +0900

    test

--- src/javaja/TestMain1.java
+++ src/javaja/TestMain1.java
@@ -3,8 +3,20 @@ package javaja;
 public class TestMain1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!!");
+		int[] arr = new int[6];
+		for(int i = 0; i < 6;i++) {
+			arr[i] = (int)(Math.random()*45)+1;
+			for(int j = 0; j < i;j++) {
+				if(arr[i]==arr[j]) {
+					i--;
+					break;
+				}
+			}}
+		
+			for(int j = 0; j < 6;j++) {
+				System.out.print(arr[j] + " ");
+		}
+		
 	}
 
 }
