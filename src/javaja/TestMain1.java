5a4445869629c16a1212aaccf3880f6813766c8e
tjqwn <tjqwn@ahn-laptop>
2024-01-16 10:16:52 +0900

    test

--- src/javaja/TestMain1.java
+++ src/javaja/TestMain1.java
@@ -4,19 +4,20 @@ public class TestMain1 {
 
 	public static void main(String[] args) {
 		int[] arr = new int[6];
-		for(int i = 0; i < 6;i++) {
-			arr[i] = (int)(Math.random()*45)+1;
-			for(int j = 0; j < i;j++) {
-				if(arr[i]==arr[j]) {
+		for (int i = 0; i < 6; i++) {
+			arr[i] = (int) (Math.random() * 45) + 1;
+			for (int j = 0; j < i; j++) {
+				if (arr[i] == arr[j]) {
 					i--;
 					break;
 				}
-			}}
-		
-			for(int j = 0; j < 6;j++) {
-				System.out.print(arr[j] + " ");
+			}
 		}
-		
+
+		for (int j = 0; j < 6; j++) {
+			System.out.print(arr[j] + "  ");
+		}
+
 	}
 
 }
